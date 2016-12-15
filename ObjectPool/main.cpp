8206078820d67c7bfbce955ee191cb834d28dde5
Dima <dmredchenko@gmail.com>
2016-12-15 20:05:02 +0300

    object_pool

---
+++ ObjectPool/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+
+class Object
+{
+    // ...
+};
+
+
+class ObjectPool
+{
+private:
+    struct PoolRecord
+    {
+        Object* instance;
+        bool    in_use;
+    };
+
+    std::vector<PoolRecord> m_pool;
+
+public:
+    Object* createNewObject()
+    {
+        for (size_t i = 0; i < m_pool.size(); ++i)
+        {
+            if (! m_pool[i].in_use)
+            {
+                // переводим объект в список используемых
+                m_pool[i].in_use = true;
+                return m_pool[i].instance;
+            }
+        }
+        // если не нашли свободный объект, то расширяем пул
+        PoolRecord record;
+        record.instance = new Object;
+        record.in_use   = true;
+
+        m_pool.push_back(record);
+
+        return record.instance;
+    }
+
+    void deleteObject(Object* object)
+    {
+        // в реальности не удаляем, а лишь помечаем, 
+        // что объект свободен
+        for (size_t i = 0; i < m_pool.size(); ++i)
+        {
+            if (m_pool[i].instance == object)
+            {
+                m_pool[i].in_use = false;
+                break;
+            }
+        }
+    }
+
+    virtual ~ObjectPool()
+    {
+        // теперь уже "по-настоящему" удаляем объекты
+        for (size_t i = 0; i < m_pool.size(); ++i)
+            delete m_pool[i].instance;
+    }
+};
+
+
+int main()
+{
+    ObjectPool pool;
+    for (size_t i = 0; i < 1000; ++i)
+    {
+        Object* object = pool.createNewObject();
+        // ...
+        pool.deleteObject(object);
+    }
+    return 0;
+}
