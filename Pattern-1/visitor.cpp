ee8ca2e29f3b0e7e610eae3395f2c9d3e871c952
dmred <dmredchenko@gmail.com>
2016-12-19 11:08:56 +0300

    Add files via upload

---
+++ Pattern-1/visitor.cpp
@@ -0,0 +1,111 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Element
+{
+public:
+	virtual void accept(class Visitor &v) = 0;
+};
+
+class This : public Element
+{
+public:
+	void accept(Visitor &v);
+	string thiss()
+	{
+		return "This";
+	}
+};
+
+class That : public Element
+{
+public:
+	void accept(Visitor &v);
+	string that()
+	{
+		return "That";
+	}
+};
+
+class TheOther : public Element
+{
+public:
+	void accept(Visitor &v);
+	string theOther()
+	{
+		return "TheOther";
+	}
+};
+
+class Visitor
+{
+public:
+	virtual void visit(This *e) = 0;
+	virtual void visit(That *e) = 0;
+	virtual void visit(TheOther *e) = 0;
+};
+
+void This::accept(Visitor &v)
+{
+	v.visit(this);
+}
+
+void That::accept(Visitor &v)
+{
+	v.visit(this);
+}
+
+void TheOther::accept(Visitor &v)
+{
+	v.visit(this);
+}
+
+class UpVisitor : public Visitor
+{
+	void visit(This *e)
+	{
+		cout << "do Up on " + e->thiss() << '\n';
+	}
+	void visit(That *e)
+	{
+		cout << "do Up on " + e->that() << '\n';
+	}
+	void visit(TheOther *e)
+	{
+		cout << "do Up on " + e->theOther() << '\n';
+	}
+};
+
+class DownVisitor : public Visitor
+{
+	void visit(This *e)
+	{
+		cout << "do Down on " + e->thiss() << '\n';
+	}
+	void visit(That *e)
+	{
+		cout << "do Down on " + e->that() << '\n';
+	}
+	void visit(TheOther *e)
+	{
+		cout << "do Down on " + e->theOther() << '\n';
+	}
+};
+
+int main()
+{
+	Element *list[] =
+	{
+		new This(), new That(), new TheOther()
+	};
+
+	UpVisitor up;      
+	DownVisitor down;  
+	for (int i = 0; i < 3; i++)
+		list[i]->accept(up);
+	for (int i = 0; i < 3; i++)
+		list[i]->accept(down);
+	system("pause");
+}
